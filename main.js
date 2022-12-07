3a8f3c73b496a926bc1811e08790c2aa6ffa41b4
Deborah L <deborah.lellouche.dl@gmail.com>
2022-12-07 20:19:58 +0200

    as a function

--- main.js
+++ main.js
@@ -1,76 +1,78 @@
 
-// search bar filtering the options on input change
+// function to append a search bar filtering the options on input change
+function appendCustomizedSearchBar(appendToElement, optionsArray, optionsType) {
+    
+    // div container of the search bar and the options
+    const searchBarContainer = document.createElement('div');
+    searchBarContainer.className = 'searchBarContainer';
 
-// arrays of the options
-let optionsArray = ['apple', 'apricot', 'banana', 'orange'];
-let filteredOptionsArray = optionsArray;
-let optionsType = 'fruits';
+    // div container of the search bar and the search icon
+    const searchBarWithIcon = document.createElement('div');
+    searchBarWithIcon.className = 'searchBarWithIcon';
 
-// no result found message
-let noResultFoundMsg = 'No result found !';
+    // search bar
+    const searchBarInputField = document.createElement('input');
+    searchBarInputField.className = 'searchBarInputField';
+    searchBarInputField.type = 'search';
+    searchBarInputField.title = 'input change options filtering search bar';
+    searchBarInputField.placeholder = 'Search ' + optionsType + '...';
 
-// div container of the search bar and the options
-const searchBarContainer = document.createElement('div');
-searchBarContainer.className = 'searchBarContainer';
-
-// div container of the search bar and the search icon
-const searchBarWithIcon = document.createElement('div');
-searchBarWithIcon.className = 'searchBarWithIcon';
-
-// search bar
-const searchBarInputField = document.createElement('input');
-searchBarInputField.className = 'searchBarInputField';
-searchBarInputField.type = 'search';
-searchBarInputField.title = 'input change options filtering search bar';
-searchBarInputField.placeholder = 'Search ' + optionsType + '...';
-
-searchBarInputField.addEventListener('input', function() {
-    // model - updating the filtered options array using the search value that has just changed
-    let searchValue = searchBarInputField.value;
-    filteredOptionsArray = optionsArray.filter(function(element) {
-        return element.startsWith(searchValue);
-    });
-    filteredOptionsArray.sort();
-
-    // view - updating the options display
-    optionsContainer.innerHTML = '';                            // clearing the options container
-    if (filteredOptionsArray.length !== 0) {                    // if there are options to display
-        filteredOptionsArray.forEach(function(element) {        // appending the options display to the options container
-            const elementDisplay = document.createElement('p');
-            elementDisplay.className = 'optionDisplay';
-            elementDisplay.innerText = element;
-            optionsContainer.append(elementDisplay);
+    searchBarInputField.addEventListener('input', function() {
+        // model - updating the filtered options array using the search value that has just changed
+        let searchValue = searchBarInputField.value;
+        let filteredOptionsArray = optionsArray.filter(function(element) {
+            return element.startsWith(searchValue);
         });
-    } else {
-        const noResultFoundDisplay = document.createElement('p');
-        noResultFoundDisplay.className = 'noResultFoundDisplay';
-        noResultFoundDisplay.innerText = noResultFoundMsg;
-        optionsContainer.append(noResultFoundDisplay);
-    }
-});
+        filteredOptionsArray.sort();
 
-// search bar icon
-const searchIcon = document.createElement('i');
-searchIcon.className = 'fa fa-search icon';
+        // view - updating the options display
+        optionsContainer.innerHTML = '';                            // clearing the options container
+        if (filteredOptionsArray.length !== 0) {                    // if there are options to display
+            filteredOptionsArray.forEach(function(element) {        // appending the options display to the options container
+                const elementDisplay = document.createElement('p');
+                elementDisplay.className = 'optionDisplay';
+                elementDisplay.innerText = element;
+                optionsContainer.append(elementDisplay);
+            });
+        } else {
+            const noResultFoundDisplay = document.createElement('p');
+            noResultFoundDisplay.className = 'noResultFoundDisplay';
+            noResultFoundDisplay.innerText = 'No result found !';
+            optionsContainer.append(noResultFoundDisplay);
+        }
+    });
 
-// appending the search bar and the search icon to the same div
-searchBarWithIcon.append(searchIcon);
-searchBarWithIcon.append(searchBarInputField);
+    // search bar icon
+    const searchIcon = document.createElement('i');
+    searchIcon.className = 'fa fa-search icon';
 
-// div container of the filtered options
-const optionsContainer = document.createElement('div');
-optionsContainer.className = 'optionsDiv';
+    // appending the search bar and the search icon to the same div
+    searchBarWithIcon.append(searchIcon);
+    searchBarWithIcon.append(searchBarInputField);
 
-// appending to the body
-searchBarContainer.append(searchBarWithIcon);
-searchBarContainer.append(optionsContainer);
-document.body.append(searchBarContainer);
+    // div container of the filtered options
+    const optionsContainer = document.createElement('div');
+    optionsContainer.className = 'optionsDiv';
 
-// options first display
-filteredOptionsArray.sort().forEach(function(element) {
-    const elementDisplay = document.createElement('p');
-    elementDisplay.className = 'optionDisplay';
-    elementDisplay.innerText = element;
-    optionsContainer.append(elementDisplay);
-});
+    // appending to the appendToElement
+    searchBarContainer.append(searchBarWithIcon);
+    searchBarContainer.append(optionsContainer);
+    appendToElement.append(searchBarContainer);
 
+    // options first display
+    let filteredOptionsArray = optionsArray;
+    filteredOptionsArray.sort().forEach(function(element) {
+        const elementDisplay = document.createElement('p');
+        elementDisplay.className = 'optionDisplay';
+        elementDisplay.innerText = element;
+        optionsContainer.append(elementDisplay);
+    });
+
+}
+
+
+let fruitsArray = ['apple', 'apricot', 'banana', 'orange'];
+appendCustomizedSearchBar(document.body, fruitsArray, 'fruits');
+
+let vegetablesArray = ['cuncumber', 'pepper', 'oignon', 'tomato'];
+appendCustomizedSearchBar(document.body, vegetablesArray, 'vegetables');
